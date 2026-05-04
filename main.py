56e8cfef3a094540cb0ef068fbbf2201e2232e60
parkash34 <ohm.parkash34@gmail.com>
2026-05-05 00:26:04 +0200

    Updated it

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 from pydantic import BaseModel, field_validator
 from typing import Optional
-
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_pinecone import PineconeVectorStore
 from pinecone import Pinecone, ServerlessSpec
 from langchain_community.document_loaders import PyPDFLoader, TextLoader
@@ -12,3 +12,4 @@ from langchain_groq import ChatGroq
 from langchain_core.tools import tool
 from langgraph.prebuilt import create_react_agent
 from database import DatabaseManager
+
