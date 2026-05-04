c9c0d6a8e9db5d8d9696df219ca9d6ed6a37a0d8
parkash34 <ohm.parkash34@gmail.com>
2026-05-05 00:16:53 +0200

    Updated it

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from pydantic import BaseModel, field_validator
+from typing import Optional
+from langchain_pinecone import PineconeVectorStore
+from pinecone import Pinecone, ServerlessSpec
